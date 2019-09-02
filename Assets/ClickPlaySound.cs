f1a27c2772fef4f43f4df93bc30cf867eb71fa04
tota5 <tota5forever@gmail.com>
2019-09-01 23:45:13 -0300

    adding wwise project

---
+++ Assets/ClickPlaySound.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AK.Wwise;
+using System;
+
+[Serializable]
+public class WwiseEventPair
+{
+    public AK.Wwise.Event m_PlaySoundEvent;
+
+    public AK.Wwise.Event m_StopSoundEvent;
+}
+public class ClickPlaySound : MonoBehaviour
+{
+    public static WwiseEventPair m_LastPlayingEvent;
+
+    [SerializeField]
+    WwiseEventPair m_SoundEvent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlaySound()
+    {
+        if(ClickPlaySound.m_LastPlayingEvent == null)
+        {
+            ClickPlaySound.m_LastPlayingEvent = m_SoundEvent;
+        }
+        else
+        {
+            Debug.Log("stopping " + ClickPlaySound.m_LastPlayingEvent.m_StopSoundEvent.Name);
+            AkSoundEngine.PostEvent(ClickPlaySound.m_LastPlayingEvent.m_StopSoundEvent.Name, gameObject);
+        }
+        AkSoundEngine.PostEvent(m_SoundEvent.m_PlaySoundEvent.Name, gameObject);
+    }
+}
